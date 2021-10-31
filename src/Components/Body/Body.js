3117b26dababebf8a6df0e812a239afa7b6d83fe
narrow-space <hasanimrantri@gmail.com>
2021-10-31 14:10:15 +0600

    ..

--- src/Components/Body/Body.js
+++ src/Components/Body/Body.js
@@ -19,304 +19,435 @@ import img17 from '../../images/pic17.jpg'
 import img18 from '../../images/pic18.jpg'
 import img19 from '../../images/pic19.jpg'
 import img20 from '../../images/pic20.jpg'
+import img21 from '../../images/ph1.jpeg'
+
+import per1 from "../../images/photogelary/per1.jpg"
+import per2 from "../../images/photogelary/per2.jpg"
+import per3 from "../../images/photogelary/per3.jpg"
+import per4 from "../../images/photogelary/per4.jpg"
+import per5 from "../../images/photogelary/per5.jpg"
+
+
 import './Body.css'
+import { Col, Container, Row } from 'react-bootstrap';
 
 const Body = () => {
-    return (
-        <div className="mt-5">
-            <h2 className="text-center my-4 text">Our Photo galery</h2>
-            <div className="container ph-galery">
-
-            
-            <div className="row gy-4 rounded rounded-4">
-              <div className="col-md-3 text-center ">
-                  
-              <div className="card-container" >
-                  <div className="box">
-                      <div className="img-box">
-                      <img src={img1} className="card-img-top text-center" alt="..." />
-                      </div>
-
-                  </div>
-                    
-                    
-                </div>
+  return (
+    <div className="mt-5">
+      <h2 className="text-center my-4 text">Our Photo galery</h2>
+      <div className="container ph-galery">
 
 
-              </div>
-              <div className="col-md-3">
-                  
-              <div className="card-container" >
+        <div className="row gy-4 rounded rounded-4">
+          <div className="col-md-3 text-center ">
+
+            <div className="card-container" >
               <div className="box">
-                      <div className="img-box">
-                    <img src={img2} className="card-img-top" alt="..." />
-
-                    
-                </div>
-                </div>
-                </div>
-
-
-              </div>
-              <div className="col-md-3">
-                  
-              <div className="card-container" >
-              <div className="box">
-                      <div className="img-box">
-                    <img src={img3} className="card-img-top" alt="..." />
-
-                    
-                </div>
-                </div>
-                </div>
-
-
-              </div>
-              <div className="col-md-3">
-                  
-              <div className="card-container" >
-              <div className="box">
-                      <div className="img-box">
-                    <img src={img4} className="card-img-top" alt="..." />
-
-                    
-                </div>
-                </div>
-                </div>
-
-
-              </div>
-              <div className="col-md-3">
-              <div className="card-container" >
-              <div className="box">
-                      <div className="img-box">
-                    <img src={img5} className="card-img-top" alt="..." />
-
-                    
-                </div>
-                </div>
-                </div>
-
-
-              </div>
-              <div className="col-md-3">
-                  
-              <div className="card-container" >
-              <div className="box">
-                      <div className="img-box">
-                    <img src={img6} className="card-img-top" alt="..." />
-
-                    
-                </div>
-                </div>
-                </div>
-
-
-              </div>
-              <div className="col-md-3">
-                  
-              <div className="card-container" >
-              <div className="box">
-                      <div className="img-box">
-                    <img src={img7} className="card-img-top" alt="..." />
-
-                    
-                </div>
-                </div>
-                </div>
-
-
-              </div>
-              <div className="col-md-3">
-                  
-              <div className="card-container" >
-              <div className="box">
-                      <div className="img-box">
-                    <img src={img8} className="card-img-top" alt="..." />
-
-                    
-                </div>
-                </div>
-                </div>
-
-
-              </div>
-              <div className="col-md-3">
-                  
-              <div className="card-container" >
-              <div className="box">
-                      <div className="img-box">
-                    <img src={img9} className="card-img-top" alt="..." />
-
-                    
-                </div>
-                </div>
-                </div>
-
-
-              </div>
-              <div className="col-md-3">
-                  
-              <div className="card-container" >
-              <div className="box">
-                      <div className="img-box">
-                    <img src={img10} className="card-img-top" alt="..." />
-
-                    
-                </div>
-                </div>
-                </div>
-
-
-              </div>
-              <div className="col-md-3">
-                  
-              <div className="card-container" >
-              <div className="box">
-                      <div className="img-box">
-                    <img src={img11} className="card-img-top" alt="..." />
-
-                    
-                </div>
-                </div>
-                </div>
-
-
-              </div>
-              <div className="col-md-3">
-                  
-              <div className="card-container" >
-              <div className="box">
-                      <div className="img-box">
-                    <img src={img12} className="card-img-top" alt="..." />
-
-                    
-                </div>
-                </div>
-                </div>
-
-
-              </div>
-              <div className="col-md-3">
-                  
-              <div className="card-container" >
-              <div className="box">
-                      <div className="img-box">
-                    <img src={img13} className="card-img-top" alt="..." />
-
-                    
-                </div>
-                </div>
-                </div>
-
-
-              </div>
-              <div className="col-md-3">
-                  
-              <div className="card-container" >
-              <div className="box">
-                      <div className="img-box">
-                    <img src={img14} className="card-img-top" alt="..." />
-
-                    
-                </div>
-                </div>
-                </div>
-
-
-              </div>
-              <div className="col-md-3">
-                  
-              <div className="card-container" >
-              <div className="box">
-                      <div className="img-box">
-                    <img src={img15} className="card-img-top" alt="..." />
-
-                    
-                </div>
-                </div>
-                </div>
-
-
-              </div>
-              <div className="col-md-3">
-                  
-              <div className="card-container" >
-              <div className="box">
-                      <div className="img-box">
-                    <img src={img16} className="card-img-top" alt="..." />
-
-                    
-                </div>
-                </div>
-                </div>
-
-
-              </div>
-              <div className="col-md-3">
-                  
-              <div className="card-container" >
-              <div className="box">
-                      <div className="img-box">
-                    <img src={img17} className="card-img-top" alt="..." />
-
-                    
-                </div>
-                </div>
-                </div>
-
-
-              </div>
-              <div className="col-md-3">
-                  
-              <div className="card-container" >
-              <div className="box">
-                      <div className="img-box">
-                    <img src={img18} className="card-img-top" alt="..." />
-
-                    
-                </div>
-                </div>
+                <div className="img-box">
+                  <img src={img1} className="card-img-top text-center" alt="..." />
                 </div>
 
               </div>
-              <div className="col-md-3">
-                  
-              <div className="card-container" >
-              <div className="box">
-                      <div className="img-box">
-                    <img src={img19} className="card-img-top" alt="..." />
-
-                    
-                </div>
-                </div>
-                </div>
-
-
-              </div>
-              <div className="col-md-3">
-                  
-              <div className="card-container" >
-              <div className="box">
-                      <div className="img-box">
-                    <img src={img20} className="card-img-top" alt="..." />
-
-                    
-                </div>
-                </div>
-                </div>
-
-
-              </div>
-             
-
-                
 
 
             </div>
+
+
+          </div>
+          <div className="col-md-3">
+
+            <div className="card-container" >
+              <div className="box">
+                <div className="img-box">
+                  <img src={img2} className="card-img-top" alt="..." />
+
+
+                </div>
+              </div>
             </div>
+
+
+          </div>
+          <div className="col-md-3">
+
+            <div className="card-container" >
+              <div className="box">
+                <div className="img-box">
+                  <img src={img3} className="card-img-top" alt="..." />
+
+
+                </div>
+              </div>
+            </div>
+
+
+          </div>
+          <div className="col-md-3">
+
+            <div className="card-container" >
+              <div className="box">
+                <div className="img-box">
+                  <img src={img4} className="card-img-top" alt="..." />
+
+
+                </div>
+              </div>
+            </div>
+
+
+          </div>
+          <div className="col-md-3">
+            <div className="card-container" >
+              <div className="box">
+                <div className="img-box">
+                  <img src={img5} className="card-img-top" alt="..." />
+
+
+                </div>
+              </div>
+            </div>
+
+
+          </div>
+          <div className="col-md-3">
+
+            <div className="card-container" >
+              <div className="box">
+                <div className="img-box">
+                  <img src={img6} className="card-img-top" alt="..." />
+
+
+                </div>
+              </div>
+            </div>
+
+
+          </div>
+          <div className="col-md-3">
+
+            <div className="card-container" >
+              <div className="box">
+                <div className="img-box">
+                  <img src={img7} className="card-img-top" alt="..." />
+
+
+                </div>
+              </div>
+            </div>
+
+
+          </div>
+          <div className="col-md-3">
+
+            <div className="card-container" >
+              <div className="box">
+                <div className="img-box">
+                  <img src={img8} className="card-img-top" alt="..." />
+
+
+                </div>
+              </div>
+            </div>
+
+
+          </div>
+          <div className="col-md-3">
+
+            <div className="card-container" >
+              <div className="box">
+                <div className="img-box">
+                  <img src={img9} className="card-img-top" alt="..." />
+
+
+                </div>
+              </div>
+            </div>
+
+
+          </div>
+          <div className="col-md-3">
+
+            <div className="card-container" >
+              <div className="box">
+                <div className="img-box">
+                  <img src={img10} className="card-img-top" alt="..." />
+
+
+                </div>
+              </div>
+            </div>
+
+
+          </div>
+          <div className="col-md-3">
+
+            <div className="card-container" >
+              <div className="box">
+                <div className="img-box">
+                  <img src={img11} className="card-img-top" alt="..." />
+
+
+                </div>
+              </div>
+            </div>
+
+
+          </div>
+          <div className="col-md-3">
+
+            <div className="card-container" >
+              <div className="box">
+                <div className="img-box">
+                  <img src={img12} className="card-img-top" alt="..." />
+
+
+                </div>
+              </div>
+            </div>
+
+
+          </div>
+          <div className="col-md-3">
+
+            <div className="card-container" >
+              <div className="box">
+                <div className="img-box">
+                  <img src={img13} className="card-img-top" alt="..." />
+
+
+                </div>
+              </div>
+            </div>
+
+
+          </div>
+          <div className="col-md-3">
+
+            <div className="card-container" >
+              <div className="box">
+                <div className="img-box">
+                  <img src={img14} className="card-img-top" alt="..." />
+
+
+                </div>
+              </div>
+            </div>
+
+
+          </div>
+          <div className="col-md-3">
+
+            <div className="card-container" >
+              <div className="box">
+                <div className="img-box">
+                  <img src={img15} className="card-img-top" alt="..." />
+
+
+                </div>
+              </div>
+            </div>
+
+
+          </div>
+          <div className="col-md-3">
+
+            <div className="card-container" >
+              <div className="box">
+                <div className="img-box">
+                  <img src={img16} className="card-img-top" alt="..." />
+
+
+                </div>
+              </div>
+            </div>
+
+
+          </div>
+          <div className="col-md-3">
+
+            <div className="card-container" >
+              <div className="box">
+                <div className="img-box">
+                  <img src={img17} className="card-img-top" alt="..." />
+
+
+                </div>
+              </div>
+            </div>
+
+
+          </div>
+          <div className="col-md-3">
+
+            <div className="card-container" >
+              <div className="box">
+                <div className="img-box">
+                  <img src={img18} className="card-img-top" alt="..." />
+
+
+                </div>
+              </div>
+            </div>
+
+          </div>
+          <div className="col-md-3">
+
+            <div className="card-container" >
+              <div className="box">
+                <div className="img-box">
+                  <img src={img19} className="card-img-top" alt="..." />
+
+
+                </div>
+              </div>
+            </div>
+
+
+          </div>
+          <div className="col-md-3">
+
+            <div className="card-container" >
+              <div className="box">
+                <div className="img-box">
+                  <img src={img20} className="card-img-top" alt="..." />
+
+
+                </div>
+              </div>
+            </div>
+
+
+          </div>
+
+
+
+
+
         </div>
-    );
+      </div>
+      <h2 className="text-center">News & Event</h2>
+      <div className="ms-5">
+
+
+        <div className="row">
+          <div className="col-md-4 ">
+            <h3 style={{ "fontFamily": "cursive", "color": "red", "textAlign": "center" }}>photo galery</h3>
+            <hr />
+            <div id="img-container" className="row ">
+              <img src={img1} alt="" />
+              <img src={img2} alt="" />
+              <img src={img3} alt="" />
+              <img src={img7} alt="" />
+              <img src={img4} alt="" />
+              <img src={img8} alt="" />
+              <img src={img10} alt="" />
+              <img src={img11} alt="" />
+              <img src={img20} alt="" />
+              <img src={img12} alt="" />
+              <img src={img15} alt="" />
+              <img src={img19} alt="" />
+            </div>
+
+          </div>
+          <div className="col-md-4">
+
+            <h2 style={{ "fontFamily": "cursive", "color": "red", }}>Vedio Gellery</h2>
+            <hr />
+            <div>
+              <iframe width="300" height="200" src="https://www.youtube.com/embed/hQ0n9gxAAmc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+            <p style={{"padding":"10px"}}  className="w-100">
+              What services do luxury hotels offer?
+              10 of the most exclusive luxury hotel services
+              Shaman sessions. American elite hotel called Post Ranch Inn offers special sessions with Shamans to all guests
+              Spa specials with your pet
+              Bedtime stories.
+              Sky taxi
+              Aroma holiday. 
+              Pigeons away!
+              Cinema spa
+              Hotel for dolls.
+            </p>
+
+
+          </div>
+          <div className="col-md-4">
+            <h2 style={{ "fontFamily": "cursive", "color": "red", }}>NEWS & EVENT</h2>
+            <hr />
+            <div className="news-events ">
+              
+             <div className="d-flex">
+             <div>
+              <img style={{"clipPath":"circle()","width":"40px"}} src={per1} alt="" />
+              </div>
+              <div className="ms-5">
+              <p>Our Happyclient</p>
+              <span>3 dec 2021</span>
+              
+              </div>
+             </div>
+             <hr />
+
+             <div className="d-flex">
+             <div>
+              <img style={{"clipPath":"circle()","width":"40px"}} src={per2} alt="" />
+              </div>
+              <div className="ms-5">
+              <p>Our Happyclient</p>
+              <span>3 dec 2021</span>
+              
+              </div>
+             </div>
+             <hr />
+
+             <div className="d-flex">
+             <div>
+              <img style={{"clipPath":"circle()","width":"40px"}} src={per3} alt="" />
+              </div>
+              <div className="ms-5">
+              <p>Our Happyclient</p>
+              <span>3 dec 2021</span>
+              
+              </div>
+             </div>
+             <hr />
+
+             <div className="d-flex">
+             <div>
+              <img style={{"clipPath":"circle()","width":"40px"}} src={per4} alt="" />
+              </div>
+              <div className="ms-5">
+              <p>Our Happyclient</p>
+              <span>3 dec 2021</span>
+              
+              </div>
+             </div>
+             <hr />
+
+
+             <div className="d-flex">
+             <div>
+              <img style={{"clipPath":"circle()","width":"40px"}} src={per5} alt="" />
+              </div>
+              <div className="ms-5 ">
+              <p>Our Happyclient</p>
+              <span>3 dec 2021</span>
+              
+              </div>
+             </div>
+             <hr />
+
+            </div>
+
+          </div>
+
+        </div>
+
+      </div>
+
+
+    </div>
+  );
 };
 
 export default Body;
